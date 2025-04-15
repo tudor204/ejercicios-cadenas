290e0e7aef03aa26c76f81894d4510287661bf0d
tudor204 <tudor204@hotmail.com>
2025-04-15 11:53:24 +0200

    Add files via upload

---
+++ 01_Ejercicio1.py
@@ -0,0 +1,9 @@
+"""
+Escribir un programa que pregunte el nombre del usuario en la consola 
+y un número entero e imprima por pantalla en líneas distintas el nombre
+ del usuario tantas veces como el número introducido.
+"""
+
+nombre = input("¿Cómo te llamas? ")
+numero = input("Introduce un número entero: ")
+print((nombre + "\n") * int(numero))
